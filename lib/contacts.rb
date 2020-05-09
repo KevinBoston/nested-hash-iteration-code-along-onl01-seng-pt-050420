b7a908aab07a173922d9b64e4916200b7ebce81c
Kevin Boston <kbos22@gmail.com>
2020-05-09 19:40:14 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,10 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person === "Freddy Mercury"
-    data.each do |k, v|
-      if k == :favorite_ice_cream_flavors
-       data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      data.each do |k, v|
+        if k == :favorite_ice_cream_flavors
+         data.delete_if {|ice_cream| ice_cream == "strawberry"}
+       end
       end
     end
   end
